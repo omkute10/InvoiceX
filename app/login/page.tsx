3f59688d8a9f261564da8e1a20968c966c1adc8f
Om Kute <kuteom2006@gmail.com>
2024-12-27 21:20:28 +0530

    Added Authentication and Prisma Client

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { signIn } from "../utils/auth";
 
 export default function login() {
     return (
@@ -20,7 +21,12 @@ export default function login() {
                         <CardDescription>Enter your email below to login into your account</CardDescription>
                     </CardHeader>
                     <CardContent>
-                        <form className="flex flex-col gap-y-4">
+                        <form 
+                            action={async () => {
+                                "use server";
+                                await signIn();
+                            }}
+                        className="flex flex-col gap-y-4">
                             <div className="flex flex-col gap-y-2">
                                 <Label>Email</Label>
                                 <Input placeholder="invoicex@gmail.com"></Input>
