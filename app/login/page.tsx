b4eacd6bc23fd534457785fa8acf40f448100b07
Om Kute <kuteom2006@gmail.com>
2025-01-01 21:23:38 +0530

    Updated Login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,9 @@
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardHeader,
-    CardTitle,
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
@@ -12,16 +12,16 @@ import { redirect } from "next/navigation";
 import { SubmitButton } from "../components/SubmitButton";
 
 export default async function login() {
-    const session = await auth();
+  const session = await auth();
 
-    if (session?.user) {
-        redirect("/dashboard");
-    }
+  if (session?.user) {
+    redirect("/dashboard");
+  }
 
-    return (
-        <>
-            <div
-                className="
+  return (
+    <>
+      <div
+        className="
                 flex
                 h-screen
                 w-full
@@ -29,36 +29,36 @@ export default async function login() {
                 justify-center
                 px-4
             "
+      >
+        <Card className="max-w-sm">
+          <CardHeader>
+            <CardTitle className="text-2xl">Login</CardTitle>
+            <CardDescription>
+              Enter your email below to login into your account
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form
+              action={async (formData) => {
+                "use server";
+                await signIn("nodemailer", formData);
+              }}
+              className="flex flex-col gap-y-4"
             >
-                <Card className="max-w-sm">
-                    <CardHeader>
-                        <CardTitle className="text-2xl">Login</CardTitle>
-                        <CardDescription>
-                            Enter your email below to login into your account
-                        </CardDescription>
-                    </CardHeader>
-                    <CardContent>
-                        <form
-                            action={async (formData) => {
-                                "use server";
-                                await signIn("nodemailer", formData);
-                            }}
-                            className="flex flex-col gap-y-4"
-                        >
-                            <div className="flex flex-col gap-y-2">
-                                <Label>Email</Label>
-                                <Input
-                                    name="email"
-                                    type="email"
-                                    required
-                                    placeholder="invoicex@gmail.com"
-                                ></Input>
-                            </div>
-                            <SubmitButton />
-                        </form>
-                    </CardContent>
-                </Card>
-            </div>
-        </>
-    );
+              <div className="flex flex-col gap-y-2">
+                <Label>Email</Label>
+                <Input
+                  name="email"
+                  type="email"
+                  required
+                  placeholder="invoicex@gmail.com"
+                ></Input>
+              </div>
+              <SubmitButton text="Login" />
+            </form>
+          </CardContent>
+        </Card>
+      </div>
+    </>
+  );
 }
