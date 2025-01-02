4c95f86b75337ce63952cdab0637a791362e992b
Om Kute <kuteom2006@gmail.com>
2025-01-02 20:40:11 +0530

    Completed onboarding page with client and server input validation

--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Card,
   CardContent,
@@ -8,8 +10,25 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { SubmitButton } from "../components/SubmitButton";
+import { useActionState } from "react";
+import { onboardUser } from "../actions";
+import { useForm } from "@conform-to/react";
+import { parseWithZod } from "@conform-to/zod";
+import { onBoardingSchema } from "../utils/zodSchemas";
 
 export default function OnBoarding() {
+  const [lastResult, action] = useActionState(onboardUser, undefined);
+  const [form, fields] = useForm({
+    lastResult,
+    onValidate({ formData }) {
+      return parseWithZod(formData, {
+        schema: onBoardingSchema,
+      });
+    },
+    shouldValidate: "onBlur",
+    shouldRevalidate: "onInput",
+  });
+
   return (
     <div className="min-h-screen w-screen flex items-center justify-center">
       <Card className="max-w-sm mx-auto">
@@ -20,22 +39,48 @@ export default function OnBoarding() {
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <form className="grid gap-4">
+          <form
+            className="grid gap-4"
+            action={action}
+            id={form.id}
+            onSubmit={form.onSubmit}
+            noValidate
+          >
             <div className="grid grid-cols-2 gap-4">
               <div className="flex flex-col gap-2">
                 <Label>First Name</Label>
-                <Input placeholder="Om"></Input>
+                <Input
+                  name={fields.firstName.name}
+                  key={fields.firstName.key}
+                  defaultValue={fields.firstName.initialValue}
+                  placeholder="Om"
+                ></Input>
+                <p className="text-red-500 text-sm">
+                  {fields.firstName.errors}
+                </p>
               </div>
               <div className="grid gap-2">
                 <Label>Last name</Label>
-                <Input placeholder="Kute"></Input>
+                <Input
+                  name={fields.lastName.name}
+                  key={fields.lastName.key}
+                  defaultValue={fields.lastName.initialValue}
+                  placeholder="Kute"
+                ></Input>
+                <p className="text-red-500 text-sm">{fields.lastName.errors}</p>
               </div>
             </div>
             <div className="grid gap-2">
-                <Label>Address</Label>
-                <Input placeholder="Pune, India"></Input>
+              <Label>Address</Label>
+              <Input
+                name={fields.address.name}
+                key={fields.address.key}
+                defaultValue={fields.address.initialValue}
+                placeholder="Pune, India"
+              ></Input>
+              <p className="text-red-500 text-sm">{fields.address.errors}</p>
             </div>
-            <SubmitButton text="Finish"/>
+            <SubmitButton text="Finish" />
           </form>
         </CardContent>
       </Card>
