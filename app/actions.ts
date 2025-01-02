6feedc046ed29c014cfb2a4002537e73b388c1c5
Om Kute <kuteom2006@gmail.com>
2025-01-02 20:41:09 +0530

    Other Updates

--- app/actions.ts
+++ app/actions.ts
@@ -1,5 +1,32 @@
 "use server";
 
-export async function onboardUser() {
-    
+import { requireUser } from "./utils/hooks";
+import { parseWithZod } from "@conform-to/zod";
+import { onBoardingSchema } from "./utils/zodSchemas";
+import prisma from "./utils/db";
+import { redirect } from "next/navigation";
+
+export async function onboardUser(prevState: any, formData: FormData) {
+  const session = await requireUser();
+
+  const submission = parseWithZod(formData, {
+    schema: onBoardingSchema,
+  });
+
+  if (submission.status !== "success") {
+    return submission.reply();
+  }
+
+  const data = await prisma.user.update({
+    where: {
+      id: session.user?.id,
+    },
+    data: {
+      firstname: submission.value.firstName,
+      lastname: submission.value.lastName,
+      address: submission.value.address,
+    },
+  });
+
+  return redirect("/dashboard");
 }
