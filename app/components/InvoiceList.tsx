73be27f9259deecc2ee3dc83df7f3d7ea9a405b0
Om Kute <kuteom2006@gmail.com>
2025-01-07 21:53:30 +0530

    Added Invoice Management Route and Designed the Table with Dropdown Menu

---
+++ app/components/InvoiceList.tsx
@@ -0,0 +1,31 @@
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { InvoiceActions } from "./InvoiceActions";
+
+export function InvoiceList() {
+    return (
+        <Table>
+            <TableHeader>
+                <TableRow>
+                    <TableHead>Invoice ID</TableHead>
+                    <TableHead>Customer</TableHead>
+                    <TableHead>Amount</TableHead>
+                    <TableHead>Status</TableHead>
+                    <TableHead>Date</TableHead>
+                    <TableHead className="text-right">Actions</TableHead>
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+                <TableRow>
+                    <TableCell>#1</TableCell>
+                    <TableCell>Om Kute</TableCell>
+                    <TableCell>$100.00</TableCell>
+                    <TableCell>Paid</TableCell>
+                    <TableCell>07/01/2025</TableCell>
+                    <TableCell className="text-right">
+                        <InvoiceActions/>
+                    </TableCell>
+                </TableRow>
+            </TableBody>
+        </Table>
+    )
+}
